a97f59bb5aee99343d166046fc9ae1aeb671beaa
Adham Khalilieh <Adham1011@github.com>
2018-09-06 05:24:53 +0300

    final edits - no testing

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -1,61 +1,124 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Geocode from "react-geocode";
 import SearchingForm from './SearchingForm';
 import { GOOGLE_KEY } from '../consts';
+import { GEO_KEY } from '../consts';
+import './css/style.css';
 
-const mapStyle  = {styles: require('../jsons/mapStyle.json')};
+
+const mapStyle  = {styles: require('../jsons/mapStyle.json'),scrollwheel: false, zoomControl:false,gestureHandling:'cooperative'};
 const key = GOOGLE_KEY;
 
 
+const geo_key = GEO_KEY;
+Geocode.setApiKey(geo_key);
+Geocode.enableDebug();
 
 
 class MainMap extends Component {
 
     static defaultProps = {
     center: {
-      lat: 41.923782,
-      lng: 25.919254
+      lat: 46.2276,
+      lng: 2.2137
     },
-    zoom: 5.03,
+    zoom: 5.72,
   };
 
-      constructor(props){
+    constructor(props){
     super(props);
     this.state = {
-      style:{
-        display: 'none',
-        left:0,
-        top:0
+      display:'block',
+       style:{
+        left:`${50}%`,
+        top:`${50}%`
       },
       geo:{
         lat:0,
-        lng:0
+        lng:0,
+        short_name:'fr',
+        long_name:'France'
       }
     }
     // this.increment = this.increment.bind(this);
   }
 
+    helpClick = ({event}) =>{
+    this.setState({
+      display:'block',
+      style:{
+        left:`${50}%`,
+        top:`${50}%`
+      },
+      geo:{
+        short_name:'fr',
+        long_name:'France'
+      }
+    });
+  }
+
+  showHide = (value)=>{
+    console.log(value);
+    this.setState({
+      display:value
+    })
+  }
+
 
   _onClick = ({x, y, lat, lng, event}) => {
-    console.log(x, y, lat, lng, event)
+
+  Geocode.fromLatLng(lat, lng).then(
+  response => {
+    console.log('okay !');
+    const address = response.results[0].address_components.filter(obj => obj.types.find(elem=>{
+      return elem === 'country';
+    }))
+    console.log(address);
+    // console.log(`Adress: ${address.long_name}`);
     this.setState({
       style:{
-        display:'block',
-        left:x-15,
-        top:y-15
+        left:x,
+        top:y+20
       },
       geo:{
         lat:lat,
-        lng:lng 
+        lng:lng,
+        long_name : address[0].long_name,
+        short_name: address[0].short_name 
       }
     });
-    // console.log(this.state.style);
+    console.log(this.state.geo);
+    console.log(`new address:${address[0].short_name}`)
+  },err=>{
+    console.log(err);
   }
+  );
+}
 
   render() {
     return (
       <div style={{ height: '100vh', width: '100%' }} >
-        <SearchingForm style={this.state.style} geo={this.state.geo}/>
+        <button className='help' onClick={this.helpClick} style={{display:(this.state.display === 'none')? 'block' : 'none' }}>help</button>
+        <h1 className='main-big-title mt-3' style={{display:(this.state.display === 'none')? 'block' : 'none' }}>LET'S TAKE<br/>A SEARCH<br/>FOR A RIDE</h1>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className='col-12 help-box' style={{display:this.state.display}}>
+              <div className=" col-12 text-center mt-5">
+                <h6 className="small-title">Google presents different results for every country.</h6>
+                <h1 className='main-title'>LET'S TAKE<br/>A SEARCH<br/>FOR A RIDE</h1>
+              </div>
+              <div className='instructions'>
+                <h6 className="small-title">
+                1. Tap your request in the search box<br/>
+                2. pick up a location and see how the result will change
+                </h6>
+              </div>
+            </div>
+            <SearchingForm style={this.state.style} geo={this.state.geo} display={this.state.display} ishelp={this.showHide}/>
+          </div>
+        </div>
         <GoogleMapReact
           onClick={this._onClick}
           options={mapStyle}
@@ -69,3 +132,7 @@ class MainMap extends Component {
 }
 
 export default MainMap;
+
+
+
+
--- src/components/SearchingForm.js
+++ src/components/SearchingForm.js
@@ -1,62 +1,133 @@
 import React, { Component } from 'react';
 import { GEO_KEY } from '../consts';
-
-
-import Geocode from "react-geocode";
+import axios from 'axios';
 
 import './css/form.css';
 
 import { Icon } from 'react-icons-kit';
 import {plusCircle} from 'react-icons-kit/fa/plusCircle';
+import pin from '../icons/pin.svg';
 
 
+
+import Geocode from "react-geocode";
 const geo_key = GEO_KEY;
 Geocode.setApiKey(geo_key);
 Geocode.enableDebug();
 
 
 
-
+var listitem=[];
 
 class SearchingForm extends Component {
   constructor(props) {
   super(props);
   this.state = { 
-    search: '' 
+    search: '',
+    C_long_name:'France',
+    location:'fr',
+    results:[],
+    isresult:'none'
   };
   this.submit = this.submit.bind(this);
   this.onChange = this.onChange.bind(this);
+  this.handleResponse= this.handleResponse.bind(this);
+  this.request= this.request.bind(this);
 
+}
+
+
+handleResponse(item,i){
+  return(
+    <div key={i} className='result-info'>
+      <a href={item.childNodes[1].childNodes[0].rawText} target="_blank">{item.childNodes[0].childNodes[0].rawText}</a>
+      <p>{item.childNodes[2].childNodes[0].rawText}</p>
+    </div>
+    );
+}
+request(location){
+  //   this.setState({
+  //     location:this.props.geo.short_name.toLowerCase(),
+  //     C_long_name:this.props.geo.long_name,
+  //     results: []
+  // });
+    axios.post('https://bubble-map.herokuapp.com/search',{
+      location:location,
+      search:this.state.search
+    })
+    .then(res=>{
+      // this.handleResponse(res.data.result)
+      this.setState({
+        results:res.data.result,
+        isresult:'block'
+      });
+      // console.log(listitem);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
 }
 submit(e){
   e.preventDefault();
-
-  Geocode.fromLatLng(this.props.geo.lat, this.props.geo.lng).then(
-  response => {
-    console.log('okay !');
-    const address = response.results[0].address_components.filter(obj => obj.types.find(elem=>{
-      return elem === 'country';
-    }));
-    console.log(address);
-  },
-  error => {
-    console.error(error);
-  }
-);
+  this.setState({
+      location:this.props.geo.short_name.toLowerCase(),
+      C_long_name:this.props.geo.long_name,
+      results: []
+  });
+  {this.request(this.props.geo.short_name.toLowerCase())}
+    // this.setState({
+    //   location:this.props.geo.short_name.toLowerCase(),
+    //   C_long_name:this.props.geo.long_name,
+    //   results: []
+    // });
+    // console.log(this.props.geo);
+    // axios.post('https://bubble-map.herokuapp.com/search',{
+    //   location:this.state.location,
+    //   search:this.state.search
+    // })
+    // .then(res=>{
+    //   // this.handleResponse(res.data.result)
+    //   this.setState({
+    //     results:res.data.result,
+    //     isresult:'block'
+    //   });
+    //   // console.log(listitem);
+    // })
+    // .catch(err=>{
+    //   console.log(err);
+    // })
 }
 
 onChange(e){
-  this.setState({[e.target.name]: e.target.value});
+  this.setState({[e.target.name]: e.target.value,isresult:'none'});
+  if(this.props.display == 'block'){
+    var value = 'none';
+    this.props.ishelp(value);
+  }
+}
+
+componentDidUpdate(prevProps){
+  if(prevProps.geo.short_name !== this.props.geo.short_name && this.state.search.length>0 && this.props.display === 'none'){
+      this.setState({
+      location:this.props.geo.short_name.toLowerCase(),
+      C_long_name:this.props.geo.long_name,
+      results: []
+    });
+    {this.request(this.props.geo.short_name.toLowerCase())}
+  }
+
 }
 
   render() {
     return (
-      <div className="main-sector" style={this.props.style}>
-        <div style={{color:'#FE523B',position:'relative',display:'inline-block'}}><Icon size={32} icon={plusCircle} /></div>
-        <div className='result'>
-          <form onSubmit={this.submit}>
+      <div className="search-result" style={this.props.style}>
+        <img src={pin} className="pin" />
+        <form onSubmit={this.submit}>
             <input type='text' placeholder="Search..." name="search" autoComplete="off" onChange={this.onChange}></input>
-          </form>
+        </form>
+        <div className="results" style={{display:(this.props.display)==='none' ? this.state.isresult : 'none'}}>
+          <h5>{this.state.C_long_name}</h5>
+          {this.state.results.map(this.handleResponse)}
         </div>
       </div>
     );
@@ -64,4 +135,14 @@ onChange(e){
 }
 
 export default SearchingForm;
+
+
+
+
+
+        // <div style={{color:'#FE523B',position:'relative',display:'inline-block'}}><Icon size={32} icon={plusCircle} />
+        // </div>
+
+
+
 
